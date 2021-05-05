98b223a071124f01f03f51cf06d889725b16872a
powercasgamer <powercasgamer@users.noreply.github.com>
2021-05-05 18:09:12 +0000

    Publish 'build/docs/javadoc/' to 'gh-pages' in 'powercasgamer/RandomUtils'

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"net.deltapvp.randomutils.utils","l":"ItemBuilder"},{"p":"net.deltapvp.randomutils.utils","l":"PlayerUtil"},{"p":"net.deltapvp.randomutils","l":"RandomUtils"},{"p":"net.deltapvp.randomutils.utils","l":"ServerUtil"},{"p":"net.deltapvp.randomutils.utils","l":"TextUtil"}]
