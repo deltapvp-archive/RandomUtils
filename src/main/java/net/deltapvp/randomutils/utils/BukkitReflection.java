bf0296c2e889a9649b29c21cfd65f737bf1b1629
powercas_gamer <cas@deltapvp.net>
2021-05-06 22:11:18 +0200

    no need for that sutff

--- src/main/java/net/deltapvp/randomutils/utils/BukkitReflection.java
+++ src/main/java/net/deltapvp/randomutils/utils/BukkitReflection.java
@@ -12,12 +12,6 @@ public class BukkitReflection {
     private static final String CRAFT_BUKKIT_PACKAGE;
     private static final String NET_MINECRAFT_SERVER_PACKAGE;
 
-    private static final Class<?> CRAFT_SERVER_CLASS;
-    private static final Method CRAFT_SERVER_GET_HANDLE_METHOD;
-
-    private static final Class<?> PLAYER_LIST_CLASS;
-    private static final Field PLAYER_LIST_MAX_PLAYERS_FIELD;
-
     private static final Class<?> CRAFT_PLAYER_CLASS;
     private static final Method CRAFT_PLAYER_GET_HANDLE_METHOD;
 
@@ -31,14 +25,6 @@ public class BukkitReflection {
             CRAFT_BUKKIT_PACKAGE = "org.bukkit.craftbukkit." + version + ".";
             NET_MINECRAFT_SERVER_PACKAGE = "net.minecraft.server." + version + ".";
 
-            CRAFT_SERVER_CLASS = Class.forName(CRAFT_BUKKIT_PACKAGE + "CraftServer");
-            CRAFT_SERVER_GET_HANDLE_METHOD = CRAFT_SERVER_CLASS.getDeclaredMethod("getHandle");
-            CRAFT_SERVER_GET_HANDLE_METHOD.setAccessible(true);
-
-            PLAYER_LIST_CLASS = Class.forName(NET_MINECRAFT_SERVER_PACKAGE + "PlayerList");
-            PLAYER_LIST_MAX_PLAYERS_FIELD = PLAYER_LIST_CLASS.getDeclaredField("maxPlayers");
-            PLAYER_LIST_MAX_PLAYERS_FIELD.setAccessible(true);
-
             CRAFT_PLAYER_CLASS = Class.forName(CRAFT_BUKKIT_PACKAGE + "entity.CraftPlayer");
             CRAFT_PLAYER_GET_HANDLE_METHOD = CRAFT_PLAYER_CLASS.getDeclaredMethod("getHandle");
             CRAFT_PLAYER_GET_HANDLE_METHOD.setAccessible(true);
@@ -48,7 +34,6 @@ public class BukkitReflection {
             ENTITY_PLAYER_PING_FIELD.setAccessible(true);
         } catch (Exception e) {
             e.printStackTrace();
-
             throw new RuntimeException("Failed to initialize Bukkit/NMS Reflection");
         }
     }
